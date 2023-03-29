cde6d2e40320c49cbb059e58eef1f2ae1b3d7972
Shiwang Rai <shiwangrai9@gmail.com>
2023-03-29 18:05:34 +0530

    Created Notes Page with React Router DOM

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,26 @@
 
+import { BrowserRouter, BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import './App.css';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import LandingPage from './screens/LandingPage/LandingPage';
+import MyNotes from './screens/MyNotes/MyNotes';
 
-const App =()=>  (
-    <>
-    <Header/>
-    <main>
-    <LandingPage/>
-    </main>
-    <Footer/>
-    
-  
-    </>    
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Header/>
+      <main>
+        <Routes>
+        <Route path="/" Component={LandingPage} exact />
+        <Route path="/mynotes" Component={()=><MyNotes/>}/>
+        </Routes>
+      </main>
+      <Footer />
+    </BrowserRouter>
   );
+}
 
 
 export default App;
--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -7,13 +7,16 @@ import {
   Navbar,
   NavDropdown,
 } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const Header=()=> {
 
   return (
     <Navbar bg="primary" expand="lg" variant="dark">
       <Container>
-        <Navbar.Brand href="/">Note Zipper</Navbar.Brand>
+      <Navbar.Brand> 
+      <Link to ='/'>Note Zipper</Link>
+      </Navbar.Brand> 
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="m-auto">
@@ -26,10 +29,11 @@ const Header=()=> {
                 </Form>
             </Nav>
           <Nav >
-            <Nav.Link href="#home">My Notes</Nav.Link>
+            <Nav.Link href="/mynotes">
+            <Link to="/mynotes">My Notes</Link>
+            </Nav.Link> 
             <NavDropdown title="Shiwang Rai" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">My Profile</NavDropdown.Item>
-              
               <NavDropdown.Divider />
               <NavDropdown.Item href="#action/3.4">
                 LogOut
